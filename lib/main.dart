75545b670644d8d956832501e4fd48782f450913
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:19:49 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animated',
+      title: 'Animated Like',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
