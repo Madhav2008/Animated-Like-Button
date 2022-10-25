7d53c4bc376007f947aec22e30621ba1cb0c41ee
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:50:42 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -30,7 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
           size: 80,
           likeCount: numberOfLikes,
           countPostion: CountPostion.bottom,
-          countDecoration: (count) {
+          countBuilder: (count) {
             return Text(data);
           },
         ),
