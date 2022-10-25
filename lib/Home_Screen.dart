e98fae95572adfae034b5526b3c8cfae47badae8
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:23:32 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -12,7 +12,8 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('ANIMATED LIKE BUTTON'),titleSpacing: 3,
+        title: Text('ANIMATED LIKE BUTTON'),
+        titleSpacing: 3,
       ),
       body: Center(),
     );
