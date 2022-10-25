4e23c25ac621a9a49659cb91d5ed06e16be474c3
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:16:25 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
-class name extends StatefulWidget {
-  name({Key? key}) : super(key: key);
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
-  State<name> createState() => _nameState();
+  State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _nameState extends State<name> {
+class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Container();
