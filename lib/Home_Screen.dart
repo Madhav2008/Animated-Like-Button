cbd8a51542ebfcd6a40f4dd1c355d8e121869b20
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:29:52 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -17,7 +17,7 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         title: const Text(
           'ANIMATED LIKE BUTTON',
-          style: TextStyle(),
+          style: TextStyle(letterSpacing: ),
         ),
         centerTitle: true,
       ),
