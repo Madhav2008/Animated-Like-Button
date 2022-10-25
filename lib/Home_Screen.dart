1255f351e60f9f98b87838613da7182ef46575a2
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:26:09 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -22,7 +22,7 @@ class _HomeScreenState extends State<HomeScreen> {
       body: const Center(
         child: LikeButton(
           size: 80,
-          animationDuration: Duration(milliseconds: ),
+          animationDuration: Duration(milliseconds: 1000),
         ),
       ),
     );
