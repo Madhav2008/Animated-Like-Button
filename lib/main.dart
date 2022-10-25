869e89104f8dfbd4f0be1ae598442f6a6dc4f0fe
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:28:14 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Animated Like Button',debugShowCheckedModeBanner: ,
+      title: 'Animated Like Button',debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.deepPurple,
       ),
