6ac7561de34f13cad82fe9ad3f014bbf3fa6c811
Madhav2008 <aroramadhav65@gmail.com>
2022-10-25 15:34:01 +0530

    Update

--- lib/Home_Screen.dart
+++ lib/Home_Screen.dart
@@ -11,7 +11,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int 
+  int numberOfLikes
   @override
   Widget build(BuildContext context) {
     return Scaffold(
